5986676f1e84f36a4205cf8fd98e9531deddcc0b
BoryslavKrakovych <91006670+BoryslavKrakovych@users.noreply.github.com>
2022-09-24 16:52:21 +0300

    Delete LaboratoryWork1Java directory

--- LaboratoryWork1Java/src/main/java/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main{
-    public static void main(String[]Args){
-        var Calculator = new StringCalculator();
-        System.out.println(StringCalculator.add("//1,2,-2"));
-    }
-}
